2d241401f437365c5a2a2c5ca1f25a3966a8512b
Martin Soto <msoto@eleven.de>
2014-03-13 11:12:24 +0100

    Fix init.el to handle the new auto-complete.

--- init.el
+++ init.el
@@ -78,17 +78,16 @@
 
 ;; Autocomplete mode.
 (add-to-list 'load-path "~/.emacs.d/lisp/auto-complete")
+(add-to-list 'load-path "~/.emacs.d/lisp/auto-complete/lib/popup")
 ; Load the default configuration.
 (require 'auto-complete-config)
 ; Make sure we can find the dictionaries.
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/lisp/auto-complete/dict")
+(ac-config-default)
 ; Additional auto-completion sources (for all modes).
 (setq-default ac-sources
               (append '(ac-source-dictionary ac-source-yasnippet)
                       ac-sources))
-(global-auto-complete-mode t)
-; Start auto-completion after 2 characters of a word.
-(setq ac-auto-start 2)
 ; Case sensitivity is important when finding matches.
 (setq ac-ignore-case nil)
 
