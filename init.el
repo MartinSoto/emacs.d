2930fd2a90b0c70e9caf1d8f1dc37687aef3844d
Martin Soto <martin.soto@cyren.com>
2015-04-20 11:47:41 +0200

    Add a hack to save the desktop every two minutes.

--- init.el
+++ init.el
@@ -28,12 +28,13 @@
 
 ;; Desktop save/auto-save.
 (require 'desktop)
-(defun custom-desktop-save ()
+(desktop-save-mode 1)
+(defun my-desktop-save ()
   (interactive)
   ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
   (if (eq (desktop-owner) (emacs-pid))
       (desktop-save desktop-dirname)))
-(add-hook 'auto-save-hook 'custom-desktop-save) '(desktop-path (quote (".")))
+(run-at-time "2 min" 60 'my-desktop-save)
 
 ;; Highlight trailing whitespace.
 (setq-default show-trailing-whitespace t)
