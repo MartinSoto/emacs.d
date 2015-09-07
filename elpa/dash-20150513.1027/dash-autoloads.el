4e98f3ffff6abfab1454a37fe4436a22ba6e8064
Martin Soto <donsoto@gmail.com>
2015-09-07 09:26:21 +0200

    Wholesale package update.

--- elpa/dash-20150513.1027/dash-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (21867 13335
-;;;;;;  526169 870000))
-
-;;;***
-
-(provide 'dash-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; dash-autoloads.el ends here
