5c4730609e6c8daacc698921cc7ccb518a600cba
Martin Soto <donsoto@gmail.com>
2017-05-07 20:32:49 +0200

    Add clj-refactor to the mixture

--- init.el
+++ init.el
@@ -175,6 +175,19 @@
 ;; format options
 (setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil))
 
+;; CIDER
+
+;; clj-refactor
+(require 'clj-refactor)
+
+(defun my-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+
 ;; Web mode
 (require 'web-mode)
 (defun web-mode-hook ()
